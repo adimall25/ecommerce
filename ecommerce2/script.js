156d8f9bb8f3738fadfab42e85973c4f21ebee5c
Adarsh Mall <51297290+adimall25@users.noreply.github.com>
2021-08-05 01:00:05 +0530

    Add files via upload

---
+++ ecommerce2/script.js
@@ -0,0 +1,311 @@
+let data=[
+    {
+        "name" : "Adav Pant",
+        "price" : 595,
+        "filename" : "adavpant.html",
+        "image" : "https://www.looksgud.in/upload/item-image/2279/1cuor/1cuor-ad-av-regular-fit-men-black-trousers_500x500_0.jpg"
+    },
+    {
+        "name":"Adidas Bag",
+        "price":799,
+        "filename":"adidasbag.html",
+        "image":"https://m.media-amazon.com/images/I/61FhFyYjyZS._AC_UL320_.jpg"
+    },
+    {
+        "name":"AG Kid Cloth",
+        "price":699,
+        "filename":"agkidcloth.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/kkbh8cw0/kids-apparel-combo/5/x/6/10-11-years-hb-0107-black-a-g-fashion-original-imafzzr9m43sjtcf.jpeg?q=50"
+    },
+    {
+        "name":"Aorus",
+        "price":150000,
+        "filename":"aorus.html",
+        "image":"https://www.gigabyte.com/FileUpload/Global/MainPage/4/innergigabyte/images/product/AORUS-VC.png"
+    },
+    {
+        "name":"Aspire",
+        "price":56990,
+        "filename":"aspire.html",
+        "image":"https://static-ecapac.acer.com/media/catalog/product/cache/a17a77e026ef2eddd3ecae104c32cc71/a/s/aspire-7_1.jpg"
+    },
+    {
+        "name":"Asus",
+        "price":150000,
+        "filename":"aus.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/klscivk0/computer/a/g/a/na-gaming-laptop-asus-original-imagyu7q2bkqcjzz.jpeg?q=70"
+    },
+    {
+        "name":"Backpack",
+        "price":2189,
+        "filename":"backpack.html",
+        "image":"https://rukminim1.flixcart.com/image/400/400/k3yrte80/backpack/2/f/e/bp-power-iv-gr-dy1972-backpack-adidas-original-imafmz8ttb2yjbkg.jpeg?q=90"
+    },
+    {
+        "name":"Bed",
+        "price":14890,
+        "filename":"bed.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/k4a7c7k0/bed/j/d/7/king-na-particle-board-bkwen00hhbs101p-flipkart-perfect-homes-original-imaf9m4fyrhzrp3g.jpeg?q=70"
+    },
+    {
+        "name":"Bed",
+        "price":10890,
+        "filename":"bed2.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/k687wy80/bed/f/g/u/king-na-particle-board-bkwen00nhbs200p-flipkart-perfect-homes-original-imafzqnfp58xjgjy.jpeg?q=70"
+    },
+    {
+        "name":"Bed",
+        "price":7890,
+        "filename":"bed3.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/jhs0o7k0/bed/g/k/2/queen-na-wrought-iron-fk-db-3142-q-furniturekraft-black-original-imaf5q3gz5zgtsh7.jpeg?q=70"
+    },
+    {
+        "name":"Belt",
+        "price":503,
+        "filename":"belt.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/k7f34i80/belt/f/x/g/one-size-bert-ubf999blk8005-men-s-belt-urban-forest-original-imafpnymzkqynfgs.jpeg?q=50"
+    },
+    {
+        "name":"Casio",
+        "price":2734,
+        "filename":"casio.html",
+        "image":"https://www.casio-intl.com/product/image/1425551973216/"
+    },
+    {
+        "name":"Casio Watch",
+        "price":2516,
+        "filename":"casiowatch.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/jfr57rk0/watch/m/g/b/ad217-casio-original-imaf45geggz8vdq7.jpeg?q=50"
+    },
+    {
+        "name":"Charger",
+        "price":349,
+        "filename":"charger.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kmax8y80/battery-charger/t/r/z/blaze-300-2-4-a-remembrand-original-imagf8sfzskmwxa2.jpeg?q=70"
+    },
+    {
+        "name":"Cooling Pad",
+        "price":1316,
+        "filename":"coolingpad.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/ke353m80/cooling-pad/6/e/f/swiss-military-laptop-cooling-fan-original-imafuuqfjwyx6v53.jpeg?q=70"
+    },
+    {
+        "name":"Data Card",
+        "price":1199,
+        "filename":"datacard.html",
+        "image":"https://images-na.ssl-images-amazon.com/images/I/6118n59hx5L._SL1500_.jpg"
+    },
+    {
+        "name":"designvilla",
+        "price":399,
+        "filename":"designvilla.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/k30h8y80/fabric/d/d/g/gau191r29-design-willa-original-imafm8juumhzjwqq.jpeg?q=50"
+    },
+    {
+        "name":"fabwomansaree",
+        "price":226,
+        "filename":"fabwomansaree.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kkyc9zk0/sari/t/z/h/free-s189392-fabwomen-unstitched-original-imagy6t3djmhgpmz.jpeg?q=70"
+    },
+    {
+        "name":"gamingheadset",
+        "price":799,
+        "filename":"gamingheadset.html",
+        "image":"https://images-na.ssl-images-amazon.com/images/I/61UMy%2BR84bL._SY450_.jpg"
+    },
+    {
+        "name":"glass",
+        "price":166,
+        "filename":"glass.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/ki7qw7k0-0/sunglass/l/e/r/58-transparent-stylish-aviator-unisex-sunglasses-night-driving-original-imafyfxejn6s6fzy.jpeg?q=50"
+    },
+    {
+        "name":"horizon",
+        "price":23000,
+        "filename":"horizon.html",
+        "image":"https://i.gadgets360cdn.com/products/large/mi-40-inch-full-hd-tv-4a-1250x800-1599465034.jpg"
+    },
+    {
+        "name":"icablekidcloth",
+        "price":314,
+        "filename":"icablekidcloth.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/kigbjbk0/kids-apparel-combo/z/b/m/12-18-months-gds01-redpolkadots-m-icable-original-imafy8kgzfqvufcy.jpeg?q=50"
+    },
+    {
+        "name":"kashmirakidscloths",
+        "price":474,
+        "filename":"kashmirakidscloths.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/kcdp5zk0/kids-apparel-combo/w/m/k/2-3-years-fancy-clothes-for-boys-aajuba-original-imaftgmytkggzzps.jpeg?q=50"
+    },
+    {
+        "name":"lgtv",
+        "price":19000,
+        "filename":"lgtv.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kdyus280/television/g/d/q/lg-32lm565bpta-32lm565bpta-original-imafurgkdrjfggbw.jpeg?q=70"
+    },
+    {
+        "name":"louispant",
+        "price":1049,
+        "filename":"louispant.html",
+        "image":"https://rukminim1.flixcart.com/image/714/857/k1v1h8w0/trouser/a/q/s/38-lptfmsspf10589-louis-philippe-original-imafhbzqc4xhue8x.jpeg?q=50"
+    },
+    {
+        "name":"mi",
+        "price":43990,
+        "filename":"mi.html",
+        "image":"https://i01.appmifile.com/webfile/globalimg/in/cms/56480BEB-3C35-D89D-1211-3C75F0720FBA.jpg"
+    },
+    {
+        "name":"mitv",
+        "price":15000,
+        "filename":"mitv.html",
+        "image":"https://5.imimg.com/data5/WN/RN/OQ/SELLER-87605925/mi-32-500x500.jpg"
+    },
+    {
+        "name":"monitor",
+        "price":17500,
+        "filename":"monitor.html",
+        "image":"https://images.samsung.com/is/image/samsung/in-curved-cf591-lc27f591fdwxxl-front-white-61226425?$720_576_PNG$"
+    },
+    {
+        "name":"oppo",
+        "price":8990,
+        "filename":"oppo.html",
+        "image":"https://www.ispyprice.com/static/nwprd_model/oppo-a5s-64gb-8997.jpg"
+    },
+    {
+        "name":"nitro",
+        "price":78000,
+        "filename":"nitro.html",
+        "image":"https://images-na.ssl-images-amazon.com/images/I/71s1LRpaprL._AC_SL1500_.jpg"
+    },
+    {
+        "name":"peterenglandpants",
+        "price":1119,
+        "filename":"peterenglandpants.html",
+        "image":"https://peterengland.imgix.net/img/app/product/1/196800-582150.jpg?auto=format&w=412"
+    },
+    {
+        "name":"petershirt",
+        "price":839,
+        "filename":"petershirt.html",
+        "image":"https://peterengland.imgix.net/img/app/product/5/557915-4621194.jpg?auto=format&a=412"
+    },
+    {
+        "name":"poco",
+        "price":10990,
+        "filename":"poco.html",
+        "image":"https://www.gizmochina.com/wp-content/uploads/2020/11/poco-m3-main-1.jpg"
+    },
+    {
+        "name":"real",
+        "price":16990,
+        "filename":"real.html",
+        "image":"https://static.digit.in/default/6cbbb219ccbf79f8e4c1fdaf43c668dc780d20dd.jpeg?tr=n-product_detail_leader_thumb"
+    },
+    {
+        "name":"realme",
+        "price":17990,
+        "filename":"realme.html",
+        "image":"https://www.91-img.com/pictures/141799-v3-realme-c20-mobile-phone-large-1.jpg"
+    },
+    {
+        "name":"router",
+        "price":999,
+        "filename":"router.html",
+        "image":"https://5.imimg.com/data5/UW/RJ/OL/SELLER-53008030/d-link-wireless-n300-router-dir-650in-4-antenna-500x500.png"
+    },
+    {
+        "name":"sofa",
+        "price":20595,
+        "filename":"sofa1.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/jqqy6q80/sofa-sectional/n/n/4/grey-chenille-nano-gr-blu-6-bharat-lifestyle-blue-grey-original-imafcp9fnhkhpwa4.jpeg?q=70"
+    },
+    {
+        "name":"sofa2",
+        "price":19295,
+        "filename":"sofa2.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/jvv9zm80/sofa-sectional/v/5/g/beige-cotton-blend-croma-3-seater-sofa-in-beige-brown-color-original-imafghzgtnyjpaum.jpeg?q=70"
+    },
+    {
+        "name":"sofa3",
+        "price":19295,
+        "filename":"sofa3.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kiqbma80-0/sofa-sectional/6/j/w/cream-polycotton-home-decorative-3-seater-ottoman-sofa-set-for-original-imafygq8rfzzbqwt.jpeg?q=70"
+    },
+    {
+        "name":"speaker",
+        "price":398,
+        "filename":"speaker.html",
+        "image":"https://rukminim1.flixcart.com/image/612/612/klwmufk0/speaker/mobile-tablet-speaker/x/o/8/new-arrive-mini-bluetooth-bo-at-speaker-with-fm-radio-memory-original-imagyxguqjfceawf.jpeg?q=70"
+    },
+    {
+        "name":"utensil1",
+        "price":1299,
+        "filename":"utensil1.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kmf7ki80/kitchen-rack/k/z/h/kitchen-rack-24x24-pr153-patelraj-original-imagfbtdhtm8nvq4.jpeg?q=70"
+    },
+    {
+        "name":"utensil2",
+        "price":199,
+        "filename":"utensil2.html",
+        "image":"https://rukminim1.flixcart.com/image/416/416/kcz4rrk0/kitchen-rack/z/d/s/03214-marketwala-original-imaftzp6zjf5ywwf.jpeg?q=70"
+    }
+]
+
+let searchBtnElement = document.querySelector('.search-btn');
+let itemInputElement = document.querySelector('#item');
+let tableElement = document.querySelector('.search-result table');
+let searchDivElement = document.querySelector('.search-result');
+
+let availableBusElements;
+
+searchBtnElement.addEventListener('click', function()
+{
+    let item = itemInputElement.value;
+    
+    if(item !== "")
+    {
+        availableBusElements = document.querySelectorAll('.available-dataItem') 
+        
+        for(let i = 0; i < availableBusElements.length; i++)
+        {
+            tableElement.style.display = 'none';
+            availableBusElements[i].remove();
+        }
+
+        
+
+        let found = false;
+        data.forEach(function(dataItem)
+        {
+            if(dataItem.name.toLowerCase() === item.toLowerCase())
+            {
+                tableElement.style.display = 'inline-block';
+                searchDivElement.style.display = 'inline-block';
+
+                found = true;
+                console.log('matched')
+                let template = `
+                <tr class = "available-dataItem">
+                    <td><img style="height:50px;width:50px;" src = "${dataItem.image}"></td>
+                    <td>${dataItem.name}</td>
+                    <td>${dataItem.price}</td>
+                    <td><a href="${dataItem.filename}">View</a></td>
+                </tr>
+                `
+                tableElement.insertAdjacentHTML('beforeend', template)
+            }
+        })
+        
+        if(!found)
+        {
+            let template = `
+            <h3 class = "available-dataItem">
+                No dataItem found!
+            </h3>
+            `
+            searchDivElement.insertAdjacentHTML('beforeend', template)
+        }
+    }
+})
